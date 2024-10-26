7e174cee9d7ed4fc7038edda24c0e925b1501604
mga44 <39274754+mga44@users.noreply.github.com>
2024-10-26 16:06:01 +0200

    implement new mapper version

--- src/main/java/org/mga44/court/vacancy/VacancyMapper.java
+++ src/main/java/org/mga44/court/vacancy/VacancyMapper.java
@@ -1,78 +1,46 @@
 package org.mga44.court.vacancy;
 
+import org.mga44.utils.FileWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class VacancyMapper {
+    private static final Pattern COURT_NUMBER_DEP = Pattern.compile(" (?=\\d+ )");
 
     private static final Logger log = LoggerFactory.getLogger(VacancyMapper.class);
 
-    private static final String APPELATION_HEADER = "w obszarze w³aœciwoœci";
-    private static final String COURT_HEADER = "S¹d Rejonowy";
-
-    public List<CourtVacancy> mapToVacancies(String text) {
-        List<String> verses = Arrays.stream(text.split("\n"))
-                .map(String::trim)
-                .toList();
-
+    public List<CourtVacancy> mapToVacancies(Map<String, List<String>> groupedByAppelation) {
         final List<CourtVacancy> vacancies = new ArrayList<>();
-        String appelation = "";
-        String court = "";
-        Iterator<String> iterator = verses.iterator();
-        while (iterator.hasNext()) {
-            String verse = iterator.next();
-            if (verse.startsWith(APPELATION_HEADER)) {
-                appelation = verse;
-                continue;
-            }
-
-            if (verse.matches("^.* \\d+ .*$")) {
-                // clasic structure "[court] [vacancy number] [department]"
-                CourtVacancy result = getCourtVacancy(verse, appelation);
-                vacancies.add(result);
-                continue;
-            }
-
-            String previousVerse = verse;
-            if (previousVerse.startsWith(COURT_HEADER)) {
-                verse = iterator.next();
-                ArrayList<Object> tmpVacancies = new ArrayList<>();
-                while (!verse.startsWith(COURT_HEADER)) {
-                    CourtVacancy result = getCourtVacancy(previousVerse + " " + verse, appelation);
-                    tmpVacancies.add(result);
-                }
-                //todo
-
-                if (tmpVacancies.isEmpty()) {
-                    log.warn("Improper structure containing verses {}", verse);
-                }
-
-            } else {
-                log.warn("Improper verse of value {}", previousVerse);
-            }
-//            CourtVacancy result = getCourtVacancy(verse, appelation);
-//            vacancies.add(result);
-
+        for (var e : groupedByAppelation.entrySet()) {
+            String appelation = e.getKey();
+            List<String> courts = e.getValue();
+            vacancies.addAll(courts.stream().map(verse -> getCourtVacancy(verse, appelation)).toList());
         }
 
+        FileWriter.writeContentsToFile(VacancyMapper.class,
+                vacancies.stream()
+                        .map(Objects::toString)
+                        .collect(Collectors.joining(System.lineSeparator()))
+        );
         return vacancies;
     }
 
-    private CourtVacancy getCourtVacancy(String verse, String appelation) {
-        String court;
-        String[] parts = verse.split(" (?=\\d+ )", 2);
-        court = parts[0].trim();
-        String[] numberAndDep = parts[1].split(" ", 2);
+    private CourtVacancy getCourtVacancy(String verse, String appellation) {
+        final String[] parts = COURT_NUMBER_DEP.split(verse, 2);
+        final String court = parts[0].trim();
+        final String[] numberAndDep = parts[1].split(" ", 2);
         return new CourtVacancy(
                 court,
                 numberAndDep[1].trim(),
                 Integer.parseInt(numberAndDep[0]),
-                appelation.trim()
+                appellation.trim()
         );
     }
 }
--- src/test/java/org/main/VacancyMapperTest.java
+++ src/test/java/org/main/VacancyMapperTest.java
@@ -8,6 +8,7 @@ import org.mga44.court.vacancy.CourtVacancy;
 import org.mga44.court.vacancy.VacancyMapper;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
@@ -16,10 +17,10 @@ class VacancyMapperTest {
     @Test
     void shouldMapInput() {
         //given
-        final String input = """
-                w obszarze w³aœciwoœci S¹du Apelacyjnego w Bia³ymstoku \s
-                S¹d Rejonowy w Gi¿ycku  1 II Wydzia³ Karny\s
-                """;
+        Map<String, List<String>> input = Map.of(
+                "w obszarze w³aœciwoœci S¹du Apelacyjnego w Bia³ymstoku",
+                List.of("S¹d Rejonowy w Gi¿ycku 1 II Wydzia³ Karny")
+        );
         //when
         List<CourtVacancy> vacancies = new VacancyMapper().mapToVacancies(input);
 
@@ -31,87 +32,5 @@ class VacancyMapperTest {
                         1,
                         "w obszarze w³aœciwoœci S¹du Apelacyjnego w Bia³ymstoku"
                 ));
-    }
-
-    @Test
-    void shouldMapMultipleInGroup() {
-        //given
-        final String input = """
-                w obszarze w³aœciwoœci S¹du Apelacyjnego w Bia³ymstoku \s
-                S¹d Rejonowy w Gi¿ycku  1 II Wydzia³ Karny\s
-                S¹d Rejonowy w Pu³tusku  2 I Wydzia³ Cywilny \s
-                                """;
-        //when
-        List<CourtVacancy> vacancies = new VacancyMapper().mapToVacancies(input);
-
-        //then
-        assertThat(vacancies).containsOnly(
-                new CourtVacancy(
-                        "S¹d Rejonowy w Gi¿ycku",
-                        "II Wydzia³ Karny",
-                        1,
-                        "w obszarze w³aœciwoœci S¹du Apelacyjnego w Bia³ymstoku"
-                ),
-                new CourtVacancy(
-                        "S¹d Rejonowy w Pu³tusku",
-                        "I Wydzia³ Cywilny",
-                        2,
-                        "w obszarze w³aœciwoœci S¹du Apelacyjnego w Bia³ymstoku"
-                )
-        );
-    }
-
-    @Test
-    void shouldHandleMultipleVacanciesInOneCourt() {
-        //given
-        String input = """
-                w obszarze w³aœciwoœci S¹du Apelacyjnego w Gdañsku\s
-                S¹d Rejonowy w Bydgoszczy\s
-                1 XII Wydzia³ Cywilny \s
-                1 IX Wydzia³ Karny
-                S¹d Rejonowy dla Krakowa-Podgórza\s
-                w Krakowie  2 XII Wydzia³ Cywilny\s
-                """;
-
-        //when
-        List<CourtVacancy> vacancies = new VacancyMapper().mapToVacancies(input);
-
-        //then
-        assertThat(vacancies).containsOnly(
-                new CourtVacancy(
-                        "S¹d Rejonowy w Bydgoszczy",
-                        "XII Wydzia³ Cywilny",
-                        1,
-                        "w obszarze w³aœciwoœci S¹du Apelacyjnego w Gdañsku"
-                ),
-                new CourtVacancy(
-                        "S¹d Rejonowy w Bydgoszczy",
-                        "IX Wydzia³ Karny",
-                        1,
-                        "w obszarze w³aœciwoœci S¹du Apelacyjnego w Gdañsku"
-                ),
-                new CourtVacancy(
-                        "S¹d Rejonowy dla Krakowa-Podgórza w Krakowie",
-                        "XII Wydzia³ Cywilny",
-                        2,
-                        "w obszarze w³aœciwoœci S¹du Apelacyjnego w Gdañsku"
-                )
-        );
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource(strings = {"Poz. 57Dziennik Urzêdowy Ministra Sprawiedliwoœci – 3 –\n"})
-    void shouldHandleErrorsGracefully(String input) {
-        //when
-        List<CourtVacancy> vacancies = new VacancyMapper().mapToVacancies(input);
-
-        //then
-        assertThat(vacancies).isEmpty();
-    }
-
-    @Test
-    void shouldHandleMultipleAppelations(){
-        fail("TBA");
     }
 }
